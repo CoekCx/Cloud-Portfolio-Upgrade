fefc55ef1be7dc5e9bb9066b0f21b6abbc00fd08
Nemanja <nemanja.ja.jovicic7@gmail.com>
2024-07-16 13:51:26 +0200

    Added UserTransaction entity and it's repository

---
+++ src/CryptoPortfolio/CryptoPortfolioService_Data/Entities/UserTransaction.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace CryptoPortfolioService_Data.Entities
+{
+    public class UserTransaction : TableEntity
+    {
+        public string SenderId { get; set; }
+        public string ReceiverId { get; set; }
+        public string CurrencyName { get; set; }
+        public double Amount { get; set; }
+        public double Fee { get; set; }
+        public DateTime TransactionDate { get; set; }
+
+        public UserTransaction()
+        {
+            PartitionKey = "UserTransaction";
+            RowKey = Guid.NewGuid().ToString();
+        }
+    }
+}
